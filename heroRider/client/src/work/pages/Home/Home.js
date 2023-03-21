22b24a1e96271fda7408a0acc9f118a022bc8725
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-03-22 03:27:52 +0600

    client setup successfully

---
+++ heroRider/client/src/work/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+
+function Home() {
+    return (
+        <div>
+            <div><Header></Header></div>
+            <div>Home</div>
+            <div><Footer></Footer></div>
+        </div>
+    )
+}
+
+export default Home
