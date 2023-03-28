b17ee50dab0ccc090f326d797ea0d88d39879b39
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-03-28 17:28:41 +0600

    cors and home page selected

--- heroRider/client/src/work/pages/Home/HomeDetails.js
+++ heroRider/client/src/work/pages/Home/HomeDetails.js
@@ -5,7 +5,7 @@ function HomeDetails() {
         <div
             className='text-center min-h-[120px] flex justify-center items-center'
         >
-            <h1 className='text-2xl'> Well come to homepage.</h1>
+            <h1 className='text-2xl '> Well come to homepage.</h1>
         </div>
     )
 }
